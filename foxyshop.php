6b16aa99e0097ae0401d432d9b69db9d11ee55ef
Marija Robinson <marija@springmail.com>
2020-08-29 18:37:40 -0500

    Update version in comment

--- foxyshop.php
+++ foxyshop.php
@@ -5,7 +5,7 @@ Plugin Name: FoxyShop
 Plugin URI: http://www.foxy-shop.com/
 Description: FoxyShop is a full integration for FoxyCart and WordPress, providing a robust shopping cart and inventory management tool.
 Author: SparkWeb Interactive, Inc.
-Version: 4.7.4
+Version: 4.7.5
 Author URI: http://www.foxy-shop.com/
 
 **************************************************************************
